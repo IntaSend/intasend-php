fb84051fc2bb9b72f6b1a192ba93b63f9eafee39
Felix Cheruiyot <felix.cheruiyot@kenyaapps.net>
2022-02-01 13:27:03 +0300

    Added customers class

---
+++ src/Customer.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace IntaSend\IntaSendPHP;
+
+class Customer
+{
+    public $first_name;
+    public $last_name;
+    public $email;
+    public $phone_number;
+    public $country;
+    public $city;
+    public $address;
+    public $state;
+    public $zipcode;
+}
